476785f5a1ee8fa65e2d5efed4a52a2f16f41fc4
Katerina <76427845+Katerinajames@users.noreply.github.com>
2024-11-08 18:48:15 +0200

    Add files via upload

---
+++ athlete6.py
@@ -0,0 +1,107 @@
+class Athlete:
+	def __str__(self):
+		print("Name :%s , Birthdate:%s ,Sportsname:%s,Score:%d, Worldrank:%d,Countparticipation:%d"%(self.get_name(),self.get_birthdate(),self.get_sportsname(),self.get_score(),self.get_worldrank(),self.get_CountParticipation()))	
+	def __init__(self,name,birthdate,sportsname,score, worldrank,countParticipation):
+		self.name=name
+		self.birthdate=birthdate
+		self.sportsname=sportsname
+		self.score=score
+		self.worldrank=worldrank 
+		self.countParticipation=countParticipation 
+	def set_name(self,name): 
+         self.name=name	 	        	
+	def set_birthdate(self ,birthdate): 
+         self.birthdate=birthdate     
+	def set_sportsname(self,sportsname): 
+         self.sportsname=sportsname	
+	def set_score(self ,score): 
+         self.score=score 	
+	def set_worldrank(self ,worldrank): 
+         self.worldrank=worldrank	  	
+	def set_CountParticipation(self ,countParticipation): 
+          self.countParticipation=countParticipation	      	 	      
+	def get_name  (self ): 
+         return self.name 	                
+	def get_birthdate(self): 
+         return self.birthdate 	
+	def get_sportsname(self ): 
+         return self.sportsname 	
+	def get_score(self ): 
+         return self.score
+	def get_worldrank(self): 
+         return  self.worldrank
+	def get_CountParticipation(self): 
+         return self.countParticipation               
+	def increase_CountParticipation(self): 
+         r=self.get_CountParticipation()+1
+         return r
+                 
+	def athleteScoreRecordLimit(self, recordLimit,  game):
+	 if game == "TRIPLE JUMP":
+	        
+               if   self.get_score() > recordLimit: 
+                 print ("Our athlete will participate in the sport event")
+               else:
+                  print ("Our athlete will not participate in the sport event") 
+	 if  game == "M100": 
+            if (self.get_score() >recordLimit): 
+                 print("Our athlete will participate in the sport event")
+            else:
+                  print ("Our athlete will not participate in the sport event")
+    
+	
+	def athleteWorldrecord(self, worldrecord,  game):
+	 if game == "M100":	        
+               if   self.get_score()== worldrecord: 
+                 print ("Performance equal to worldrecord")
+               if   self.get_score()> worldrecord: 
+                 print ("made a new world record")
+               if   self.get_score()< worldrecord: 
+                 print ("performance worse than the world record") 
+	 
+	 
+	 
+	 if game == "TRIPLE JUMP":	        
+               if   self.get_score()== worldrecord: 
+                 print ("Our athlete's performance is equal to worldrecord")
+               if   self.get_score()> worldrecord: 
+                 print ("Our athlete made a new world record")
+               if   self.get_score()< worldrecord: 
+                 print ("Our athlete's performance is  worse than the world record")
+             
+					     
+	
+    
+
+
+
+
+
+
+	           
+           	
+print("-----------------------------------------------------------")   	
+		
+print("ATHLETE a1  INFO")		  
+a1 =Athlete("Lewis","01/07/1961", "M100",2,2,3)
+a1.__str__()  
+print("-----------------------------------------------------------")  
+print("ATHLETE a2  INFO")
+            
+a2 =Athlete("Jones","01/08/1962", "TRIPLE JUMP",2,2,3)
+a2.__str__()                 
+       
+print("-----------------------------------------------------------") 
+print ("Call method athleteScoreRecordLimit  to check if our athlete (a1) can participate in M100")
+
+
+a1.athleteScoreRecordLimit(1,"M100")
+
+print("-----------------------------------------------------------")
+print ("Call method athleteScoreRecordLimit  to check if our athlete (a2) can participate in TRIPLE JUMP")  
+              
+                
+a2.athleteScoreRecordLimit(1.50,"TRIPLE JUMP")
+print ("Call method athleteWorldrecord to check if our athlete (a2) broke  the world record in TRIPLE JUMP")
+
+a2.athleteWorldrecord( 2,"TRIPLE JUMP"  )
